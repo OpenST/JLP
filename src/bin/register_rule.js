753caa4b6485b519ec5cda44aae4850bbfe548b0
abhayks1 <abhay@ost.com>
2019-03-07 19:50:19 +0530

    Registration of rules documentation update

--- src/bin/register_rule.js
+++ src/bin/register_rule.js
@@ -29,8 +29,8 @@ program
       await tokenRules.registerRule(ruleName, ruleAddress, ruleAbi, registerRuleTxOptions);
       const rule = await tokenRules.getRuleByName(ruleName);
       if (!rule || (rule.address !== ruleAddress)) {
-        logger.info(`Failure is registration of rule: ${ruleName}.`);
-        Promise.reject(new Error(`Failure is registration of rule: ${ruleName}.`));
+        logger.info(`Failure in registration of rule: ${ruleName}.`);
+        Promise.reject(new Error(`Failure in registration of rule: ${ruleName}.`));
       } else {
         logger.info(`Rule: ${ruleName} successfully registered.`);
       }
@@ -42,13 +42,13 @@ program
       console.log('');
       console.log('Arguments:');
       console.log('  config       path to a config file');
-      console.log('  ruleName  Rule name to be registered');
-      console.log('  ruleAddress   Rule address to be registered');
-      console.log('  ruleAbi   Abi of the rule');
+      console.log('  ruleName     Rule name to be registered');
+      console.log('  ruleAddress  Rule address to be registered');
+      console.log('  ruleAbi      Abi of the rule');
       console.log('');
       console.log('Examples:');
       console.log('  Register rule in TokenRules:');
-      console.log('  $ register_rule.js config.json rulename 0xa4aa50fbd4767085705db09e020a781e58e2fbf2 ruleAbi');
+      console.log('  $ register_rule.js config.json ruleName 0xa4aa50fbd4767085705db09e020a781e58e2fbf2 ruleAbi');
     },
   )
   .parse(process.argv);
