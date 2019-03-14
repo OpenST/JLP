92939975ef8c095df4bbdb2cf56af42e9b201800
abhayks1 <abhay@ost.com>
2019-03-14 14:22:02 +0530

    PR Review changes

--- src/bin/create_user.js
+++ src/bin/create_user.js
@@ -6,7 +6,7 @@ const program = require('commander');
 const Web3 = require('web3');
 const OpenST = require('@openstfoundation/openst.js');
 
-const ChainConfig = require('../config/chain_config');
+const connected = require('../connected');
 const logger = require('../logger');
 const { version } = require('../../package.json');
 
@@ -25,47 +25,51 @@ program
       threshold,
       sessionKeys,
       sessionKeySpendingLimits,
-      sessionKeyExpirationHeights
+      sessionKeyExpirationHeights,
     ) => {
-      const chainConfig = new ChainConfig(config);
-      const createUserTxOptions = {
-        from: chainConfig.auxiliaryMasterKey,
-        gasPrice: chainConfig.auxiliaryGasPrice,
-      };
-      const auxiliaryWeb3 = new Web3(chainConfig.auxiliaryWeb3Provider);
-      const userHelper = new OpenST.Helpers.User(
-        chainConfig.openst.tokenHolderMasterCopy,
-        chainConfig.openst.gnosisSafeMasterCopy,
-        chainConfig.openst.recoveryMasterCopy,
-        chainConfig.openst.createAndAddModules,
-        eip20Token,
-        chainConfig.openst.tokenRules,
-        chainConfig.openst.userWalletFactory,
-        chainConfig.openst.proxyFactory,
-        auxiliaryWeb3,
+      await connected.run(
+        config,
+        async (chainConfig, connection) => {
+          const createUserTxOptions = {
+            from: connection.auxiliaryAccount.address,
+            gasPrice: chainConfig.auxiliaryGasPrice,
+          };
+          const auxiliaryWeb3 = new Web3(connection.auxiliaryWeb3);
+          const userHelper = new OpenST.Helpers.User(
+            chainConfig.openst.tokenHolderMasterCopy,
+            chainConfig.openst.gnosisSafeMasterCopy,
+            chainConfig.openst.recoveryMasterCopy,
+            chainConfig.openst.createAndAddModules,
+            eip20Token,
+            chainConfig.openst.tokenRules,
+            chainConfig.openst.userWalletFactory,
+            chainConfig.openst.proxyFactory,
+            auxiliaryWeb3,
+          );
+          const ownersArray = owners.split(',').map(item => item.trim());
+          const sessionKeysArray = sessionKeys.split(',').map(item => item.trim());
+          const sessionKeySpendingLimitsArray = sessionKeySpendingLimits.split(',').map(item => item.trim());
+          const sessionKeyExpirationHeightsArray = sessionKeyExpirationHeights.split(',').map(item => item.trim());
+          const response = await userHelper.createUserWallet(
+            ownersArray,
+            threshold,
+            chainConfig.openst.recoveryOwnerAddress,
+            chainConfig.openst.recoveryControllerAddress,
+            chainConfig.openst.recoveryBlockDelay,
+            sessionKeysArray,
+            sessionKeySpendingLimitsArray,
+            sessionKeyExpirationHeightsArray,
+            createUserTxOptions,
+          );
+          const { returnValues } = response.events.UserWalletCreated;
+          const gnosisSafeProxy = returnValues._gnosisSafeProxy;
+          const tokenHolderProxy = returnValues._tokenHolderProxy;
+          logger.info('User created!');
+          const gnosisSafe = new GnosisSafe(auxiliaryWeb3, gnosisSafeProxy);
+          const modules = await gnosisSafe.getModules();
+          logger.info(`gnosisSafeProxy: ${gnosisSafeProxy}\n tokenHolderProxy: ${tokenHolderProxy}\n recoveryProxy: ${modules[0]}`);
+        },
       );
-      const ownersArray = owners.split(',').map(item => item.trim());
-      const sessionKeysArray = sessionKeys.split(',').map(item => item.trim());
-      const sessionKeySpendingLimitsArray = sessionKeySpendingLimits.split(',').map(item => item.trim());
-      const sessionKeyExpirationHeightsArray = sessionKeyExpirationHeights.split(',').map(item => item.trim());
-      const response = await userHelper.createUserWallet(
-        ownersArray,
-        threshold,
-        chainConfig.openst.recoveryOwnerAddress,
-        chainConfig.openst.recoveryControllerAddress,
-        chainConfig.openst.recoveryBlockDelay,
-        sessionKeysArray,
-        sessionKeySpendingLimitsArray,
-        sessionKeyExpirationHeightsArray,
-        createUserTxOptions,
-      );
-      const { returnValues } = response.events.UserWalletCreated;
-      const gnosisSafeProxy = returnValues._gnosisSafeProxy;
-      const tokenHolderProxy = returnValues._tokenHolderProxy;
-      logger.info('User created!');
-      const gnosisSafe = new GnosisSafe(auxiliaryWeb3, gnosisSafeProxy);
-      const modules = await gnosisSafe.getModules();
-      logger.info(`gnosisSafeProxy: ${gnosisSafeProxy}\n tokenHolderProxy: ${tokenHolderProxy}\n recoveryProxy: ${modules[0]}`);
     },
   )
   .on(
