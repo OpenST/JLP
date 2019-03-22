d463a522e0d129656780dae63169b6cdf718325e
abhayks1 <abhay@ost.com>
2019-03-22 17:35:47 +0530

    direct transfer under test framework is in progress

--- test/openst/03_direct_transfer.js
+++ test/openst/03_direct_transfer.js
@@ -3,6 +3,7 @@
 const { assert } = require('chai');
 const BN = require('bn.js');
 const { ContractInteract } = require('@openst/mosaic.js');
+const BrandedToken = require('@openst/brandedtoken.js');
 
 const { chainConfig, connection } = require('../shared');
 const OpenST = require('../../src/openst');
@@ -12,10 +13,9 @@ describe('Direct Transfer', async () => {
     const testAddresses = {
       organization: '0x0000000000000000000000000000000000000001',
       utilityToken: '0x0000000000000000000000000000000000000002',
-      owner: '0x0000000000000000000000000000000000000003',
-      sessionKey: '0x0000000000000000000000000000000000000004',
       beneficiary: '0x0000000000000000000000000000000000000005',
     };
+    const sessionKey = connection.auxiliaryAccount.address;
     const openst = new OpenST(chainConfig, connection);
     //  setupOpenst updates chainConfig with OpenST master copies and factory contracts.
     await openst.setupOpenst(testAddresses.organization, testAddresses.utilityToken);
@@ -24,14 +24,25 @@ describe('Direct Transfer', async () => {
       tokenHolderProxy,
     } = await openst.createUserWallet(
       testAddresses.utilityToken,
-      testAddresses.owner,
-      1,
-      [testAddresses.sessionKey],
+      connection.auxiliaryAccount.address,
+      1, // gnosis multisig requirement
+      [sessionKey],
       [10000000000],
       [10000000000],
     );
 
-    // TODO Fund tokenHolderProxy
+    const utilityBrandedToken = BrandedToken.ContractInteract.UtilityBrandedToken(
+      connection.auxiliaryWeb3,
+      testAddresses.utilityToken,
+    );
+    const txOptions = {
+      gasPrice: chainConfig.auxiliaryGasPrice,
+      from: connection.auxiliaryAccount.address,
+    };
+    await utilityBrandedToken.registerInternalActors(
+      [tokenHolderProxy, testAddresses.beneficiary],
+      txOptions,
+    );
     const eip20Instance = new ContractInteract.EIP20Token(
       connection.auxiliaryWeb3,
       testAddresses.utilityToken,
@@ -39,7 +50,7 @@ describe('Direct Transfer', async () => {
     const beneficiaryBalanceBefore = await eip20Instance.balanceOf(testAddresses.beneficiary);
     const transferAmount = new BN('1000');
     await openst.directTransfer(
-      testAddresses.sessionKey,
+      sessionKey,
       tokenHolderProxy,
       [testAddresses.beneficiary],
       [transferAmount],
