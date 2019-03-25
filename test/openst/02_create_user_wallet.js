639c11dda5a9833dcdf5968d79047fe6d4fa3a21
abhayks1 <abhay@ost.com>
2019-03-25 19:47:14 +0530

    register tokenholder as internal actor needed for direct transfer

--- test/openst/02_create_user_wallet.js
+++ test/openst/02_create_user_wallet.js
@@ -1,20 +1,20 @@
 // 'use strict';
 
 const { assert } = require('chai');
+const BrandedToken = require('@openst/brandedtoken.js');
 
 const { chainConfig, connection } = require('../shared');
 const OpenST = require('../../src/openst');
 
 describe('CreateUserWallet', async () => {
   it('Creates user wallet', async () => {
-    const auxiliaryOrganization = chainConfig.utilitybrandedtokens[0].organizationAddress;
     const auxiliaryUtilityToken = chainConfig.utilitybrandedtokens[0].address;
 
     const owner = connection.auxiliaryAccount.address;
     const openst = new OpenST(chainConfig, connection);
-    //  setupOpenst updates chainConfig with OpenST master copies and factory contracts.
-    await openst.setupOpenst(auxiliaryOrganization, auxiliaryUtilityToken);
 
+    // Openst Setup has already been done in openst setup smoke test.
+    // Master copies will be read from chainConfig.
     // For createUserWallet config should have recoveryOwnerAddress, recoveryControllerAddress
     // and recoveryBlockDelay
     chainConfig.openst.update({
@@ -33,6 +33,20 @@ describe('CreateUserWallet', async () => {
       connection.auxiliaryAccount.address, // Comma separated session keys.
       '100000000000',
       '100000000000',
+    );
+
+    // Register tokenHolder as internal actor.
+    const utilityBrandedToken = BrandedToken.ContractInteract.UtilityBrandedToken(
+      connection.auxiliaryWeb3,
+      auxiliaryUtilityToken,
+    );
+    const txOptions = {
+      gasPrice: chainConfig.auxiliaryGasPrice,
+      from: connection.auxiliaryAccount.address,
+    };
+    await utilityBrandedToken.registerInternalActors(
+      [tokenHolderProxy],
+      txOptions,
     );
 
     assert.isNotNull(tokenHolderProxy, 'TokenHolder proxy address should not be null.');
