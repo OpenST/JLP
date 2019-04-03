17c3d7fb8a858ecf561b32daefafbf6faf8cf891
Sarvesh Jain <sarvesh@ost.com>
2019-04-03 17:36:12 +0530

    Comming with redeem contineously working

--- src/bin/facilitator.js
+++ src/bin/facilitator.js
@@ -3,12 +3,19 @@
 'use strict';
 
 const program = require('commander');
+const Web3 = require('web3');
+const Mosaic = require('@openst/mosaic.js');
+
+const { ContractInteract } = Mosaic;
 
 const connected = require('../connected');
 const Facilitator = require('../facilitator.js');
+const StateRootAnchorService = require('../state_root_anchor_service.js');
 const logger = require('../logger');
 
 const { version } = require('../../package.json');
+
+const { BN } = Web3.utils;
 
 program
   .version(version)
@@ -67,16 +74,20 @@ program.command('redeem <config> <redeemer> <amount> <beneficiary>')
       await connected.run(
         configPath,
         async (chainConfig, connection) => {
-          const facilitator = new Facilitator(chainConfig, connection);
+          try {
+            const mosaic = chainConfig.toMosaic(connection);
+            const facilitator = new Facilitator(chainConfig, connection, mosaic);
+            const {
+              messageHash,
+              unlockSecret,
+            } = await facilitator.redeem(redeemer, amount, beneficiary);
 
-          const {
-            messageHash,
-            unlockSecret,
-          } = await facilitator.redeem(redeemer, amount, beneficiary);
-
-          logger.info(`  messageHash ${messageHash}`);
-          logger.info(`  unlockSecret ${unlockSecret}`);
-          chainConfig.write(configPath);
+            logger.info(`  messageHash ${messageHash}`);
+            logger.info(`  unlockSecret ${unlockSecret}`);
+            chainConfig.write(configPath);
+          } catch (e) {
+            console.log('exception in redeem  ', e);
+          }
         },
       );
     },
@@ -88,14 +99,80 @@ program.command('progressRedeem <config> <messageHash>')
       await connected.run(
         configPath,
         async (chainConfig, connection) => {
-          const facilitator = new Facilitator(chainConfig, connection);
+          try {
+            const mosaic = chainConfig.toMosaic(connection);
+            const facilitator = new Facilitator(chainConfig, connection, mosaic);
 
-          await facilitator.progressRedeem(messageHash);
+            await facilitator.progressRedeem(messageHash);
+          } catch (e) {
+            console.log('error in progress redeem ', e);
+          }
         },
       );
     },
   );
 
+program.command('continuousRedeem <config>'
+  + ' <utilityTokenAddress> <totalRedeemAmount> <redeemer> <beneficiary>'
+  + ' <gasPrice> <gasLimit> <minRedeemAmount> <maxRedeemAmount>')
+  .action(
+    async (
+      configPath,
+      utilityTokenAddress,
+      totalRedeemAmount,
+      redeemer,
+      beneficiary,
+      gasPrice,
+      gasLimit,
+      minRedeemAmount,
+      maxRedeemAmount,
+    ) => {
+      await connected.run(
+        configPath,
+        async (chainConfig, connection) => {
+          try {
+            const utilityToken = new ContractInteract.EIP20Token(
+              connection.auxiliaryWeb3,
+              utilityTokenAddress,
+            );
+            const mosaic = mosaicInstance(connection, chainConfig, utilityTokenAddress);
+            const facilitator = new Facilitator(chainConfig, connection, mosaic);
+            let currentBalance = new BN((await utilityToken.balanceOf(redeemer)));
+
+            if (currentBalance.gt(new BN(totalRedeemAmount))) {
+              let amount = randomNumberBetweenRange(
+                parseInt(minRedeemAmount, 10),
+                parseInt(maxRedeemAmount, 10),
+              );
+              let amountRedeemed = amount;
+              while (amountRedeemed.lten(totalRedeemAmount)) {
+                const { messageHash } = await facilitator.redeem(
+                  redeemer,
+                  amount.toString(10),
+                  beneficiary,
+                );
+                chainConfig.write(configPath);
+                console.log('message hash  ', messageHash);
+
+                await anchorAuxiliaryStateRoot(connection, chainConfig);
+                await facilitator.progressRedeem(messageHash);
+                chainConfig.write(configPath);
+                currentBalance = currentBalance.sub(amount);
+                amount = randomNumberBetweenRange(
+                  parseInt(minRedeemAmount, 10),
+                  parseInt(maxRedeemAmount, 10),
+                );
+                amountRedeemed = amountRedeemed.add(amount);
+
+              }
+            }
+          } catch (e) {
+            console.log('Exception in continuous redeem ', e);
+          }
+        },
+      );
+    },
+  );
 program.on(
   '--help',
   () => {
@@ -124,3 +201,56 @@ program.on(
 );
 
 program.parse(process.argv);
+
+function randomNumberBetweenRange(maxRedeemAmount, minRedeemAmount) {
+  const range = maxRedeemAmount - minRedeemAmount;
+  return new BN(Math.floor(Math.random() * range) + minRedeemAmount);
+}
+
+function mosaicInstance(connection, chainConfig, utilityTokenAddress) {
+  const utilityTokenConfig = chainConfig.utilityBrandedTokens.find(
+    ut => ut.address === utilityTokenAddress,
+  );
+
+  const originChain = new Mosaic.Chain(
+    connection.originWeb3,
+    {
+      Organization: chainConfig.brandedToken.originOrganization,
+      EIP20Gateway: utilityTokenConfig.originGatewayAddress,
+      Anchor: chainConfig.originAnchorAddress,
+      EIP20Token: chainConfig.eip20TokenAddress,
+    },
+  );
+  const auxiliaryChain = new Mosaic.Chain(
+    connection.auxiliaryWeb3,
+    {
+      Organization: utilityTokenConfig.organizationAddress,
+      EIP20CoGateway: utilityTokenConfig.auxiliaryCoGatewayAddress,
+      Anchor: chainConfig.auxiliaryAnchorAddress,
+      UtilityToken: utilityTokenAddress,
+    },
+  );
+
+  return new Mosaic(originChain, auxiliaryChain);
+}
+
+async function anchorAuxiliaryStateRoot(connection, chainConfig) {
+  const targetTxOptions = {
+    from: connection.originAccount.address,
+    gasPrice: chainConfig.originGasPrice,
+  };
+
+  const timeout = 1000;
+  const delay = 0;
+  const stateRootAnchorService = new StateRootAnchorService(
+    Number.parseInt(delay, 10),
+    connection.auxiliaryWeb3,
+    connection.originWeb3,
+    chainConfig.originAnchorAddress,
+    targetTxOptions,
+    timeout,
+  );
+
+  const anchorInfo = await stateRootAnchorService.getSourceInfo('latest');
+  await stateRootAnchorService.anchor(anchorInfo, targetTxOptions);
+}
